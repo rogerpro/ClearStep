be97ce007e9c88528d811b5851027a3f8acc6828
Roger Campanera <git@roger.pro>
2017-08-13 11:43:50 +0000

    Create 3 records

--- tests/Fixture/ProjectsFixture.php
+++ tests/Fixture/ProjectsFixture.php
@@ -197,16 +197,40 @@ class ProjectsFixture extends TestFixture
      */
     public $records = [
         [
-            'id' => '0a6e9bc0-70f2-4362-99d7-85d07e7c56dd',
-            'client_id' => 'a5167e31-18d0-42e9-af4d-df5128c16593',
-            'name' => 'Lorem ipsum dolor sit amet',
+            'id' => '240b37b1-8a85-4ed6-b601-c343ee704642',
+            'client_id' => '06c85706-ff99-44da-9868-02ea920d670a',
+            'name' => 'Lorem ipsum first',
             'description' => 'Lorem ipsum dolor sit amet',
             'billable' => 1,
             'hourly_price' => 1,
             'expected_hours' => 1,
             'active' => 1,
-            'created' => 1502560068,
-            'modified' => 1502560068
+            'created' => 1502624336,
+            'modified' => 1502624336
+        ],
+        [
+            'id' => 'abb8b1ad-f720-4395-86a6-d4679bcdff37',
+            'client_id' => '39d0c495-b1e0-4314-af6c-0b13d4d6d906',
+            'name' => 'Lorem ipsum second',
+            'description' => 'Lorem ipsum dolor sit amet',
+            'billable' => 1,
+            'hourly_price' => 2,
+            'expected_hours' => 2,
+            'active' => 1,
+            'created' => 1502624336,
+            'modified' => 1502624336
+        ],
+        [
+            'id' => 'cd5db656-176f-4ca4-92de-3e76c61fdb2a',
+            'client_id' => '308fc220-115a-4e51-b7f9-9143c1b1aa99',
+            'name' => 'Lorem ipsum third',
+            'description' => 'Lorem ipsum dolor sit amet',
+            'billable' => 1,
+            'hourly_price' => 3,
+            'expected_hours' => 3,
+            'active' => 1,
+            'created' => 1502624336,
+            'modified' => 1502624336
         ]
     ];
 }
